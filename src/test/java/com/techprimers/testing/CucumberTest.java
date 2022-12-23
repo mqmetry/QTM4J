b64e2c78881caa974b8ad3dbe0ba11b64f50accc
mqmetry <119501023+mqmetry@users.noreply.github.com>
2022-12-23 11:14:05 +0530

    Update CucumberTest.java

--- src/test/java/com/techprimers/testing/CucumberTest.java
+++ src/test/java/com/techprimers/testing/CucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
-        glue = "com.techprimers.testing",
+        glue = "src/test/java/com/techprimers/testing/FizzBuzzStepdefs",
         format = {"json:target/cucumber.json"}
 )
 public class CucumberTest {
