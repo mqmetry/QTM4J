e6ac154774f2d315c438eeb4e4c161d15aac8d8c
Mihir Lakhtaria <mihir.lakhtaria@qmetry.com>
2022-12-23 12:02:26 +0530

    CHance 1

--- src/test/java/FizzBuzzStepdefs.java
+++ src/test/java/FizzBuzzStepdefs.java
@@ -1,7 +1,7 @@
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
+
 public class FizzBuzzStepdefs {
 
     int num1;
