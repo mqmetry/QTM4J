225f1d616486d3f51cacbec77e11dddc524e711d
mqmetry <119501023+mqmetry@users.noreply.github.com>
2022-12-23 14:47:27 +0530

    Update CucumberTest.java

--- src/test/java/com/techprimers/testing/CucumberTest.java
+++ src/test/java/com/techprimers/testing/CucumberTest.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources",
         glue = "com.techprimers.testing",
         publish = true,
-    	monochrome = true,
-         format = {"json:target/cucumber.json"}
+        
+
 
 )
 public class CucumberTest {
