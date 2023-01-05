494f3587bba349f639d78a19fd4c6de9384ac7bf
mqmetry <119501023+mqmetry@users.noreply.github.com>
2023-01-05 17:23:58 +0530

    Update FizzBuzzTest.java

--- src/test/java/com/techprimers/testing/FizzBuzzTest.java
+++ src/test/java/com/techprimers/testing/FizzBuzzTest.java
@@ -22,7 +22,7 @@ public class FizzBuzzTest {
 
     @Test
     public void testFizz() {
-        String fizzBuzz = fB.play("Hello");
+        String fizzBuzz = fB.play(3);
         Assert.assertEquals(fizzBuzz, "Fizz");
     }
 
